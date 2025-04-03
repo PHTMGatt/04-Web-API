c53dfd47d07038c581c6e4f67cd9d2e9b020807d
PHTMGatt <phtmgatt@gmail.com>
2025-04-02 23:04:11 -0400

    main

---
+++ assets/js/form.js
@@ -0,0 +1,29 @@
+// TODO: Create a variable that selects the form element
+
+const form = document.querySelector('form');
+
+// TODO: Create a function that handles the form submission. Grab the form data and store it in local storage, then redirect to the blog page using the `redirectPage` function. If the form is submitted with missing data, display an error message to the user.
+
+const handleFormSubmit = function (event) {
+    console.log('form submitted');
+    event.preventDefault();
+    
+    const username = document.querySelector('#username').value;
+    const title = document.querySelector('#title').value;
+    const content = document.querySelector('#content').value;
+    const pTag = document.querySelector('p');
+
+    if (title === '' || content === '') {
+        pTag.textContent = 'Please complete the form.';
+        return;
+    }
+    
+    
+    storeLocalStorage({ username, title, content });
+    
+    redirectPage('blog.html');
+    };
+
+// TODO: Add an event listener to the form on submit. Call the function to handle the form submission.
+
+form.addEventListener('submit', handleFormSubmit);
